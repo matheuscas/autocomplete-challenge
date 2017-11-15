be3122dd120ec73f4a085344c6350655d036ed83
Matheus Cardoso <matheus.mcas@gmail.com>
2017-11-15 18:18:53 -0300

    post endpoint

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import asyncio
 import uvloop
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text, json
+from blueprints import db
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = Sanic(__name__)
+app.register_blueprint(db.bp_db)
 
 @app.route("/")
-async def test(request):
+async def index(request):
     return text('Desafio Dito')
+
+@app.post("/navigation/info")
+async def set_nav_info(request):
+    await db.insert_nav_info(app, {'event': request.json['event'], 'timestamp': request.json['timestamp']})
+    return json({'response': 'ok'});
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
